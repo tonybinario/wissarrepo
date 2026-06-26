86220379df5cd33b989c52c32388208937409743
antonio <s84644@bht-berlin.de>
2026-06-26 19:23:15 +0200

    m

--- models/userrepo.js
+++ models/userrepo.js
@@ -18,7 +18,6 @@ class UserRepository {
 
     // Holt einen einzelnen User anhand seiner ID
     async getUserById(id) {
-        console.log("enter");
         const query = 'SELECT id, household_id, email, first_name, last_name, phone, address, city, postal_code, role, status FROM users WHERE id = $1';
         const res = await this.pool.query(query, [id]);
 
